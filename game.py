e55d4eccc4b36ba4440ce7788ddeefa27bf08e28
lightarum <light_a@mail.ru>
2022-11-19 17:41:23 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+number = np.random.randint(1, 101) # загадываем число
+count = 0
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100:  808"))
+    if predict_number > number:
+        print("Число должно быть меньше!")
+
+    elif predict_number < number:
+        print("Число должно быть больше!")
+
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break # конец игры, выход из цикла
